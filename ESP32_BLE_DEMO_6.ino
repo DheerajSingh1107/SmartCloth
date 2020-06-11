345db7ed0f87167549dede83ddd17c5dc970a210
DheerajSingh1107 <Dheerajsingh1107@gmail.com>
2020-06-10 23:29:43 -0700

    Add files via upload

---
+++ ESP32_BLE_DEMO_6.ino
@@ -0,0 +1,194 @@
+/*
+    Video: https://www.youtube.com/watch?v=oCMOYS71NIU
+    Based on Neil Kolban example for IDF: https://github.com/nkolban/esp32-snippets/blob/master/cpp_utils/tests/BLE%20Tests/SampleNotify.cpp
+    Ported to Arduino ESP32 by Evandro Copercini
+
+   Create a BLE server that, once we receive a connection, will send periodic notifications.
+   The service advertises itself as: 6E400001-B5A3-F393-E0A9-E50E24DCCA9E
+   Has a characteristic of: 6E400002-B5A3-F393-E0A9-E50E24DCCA9E - used for receiving data with "WRITE" 
+   Has a characteristic of: 6E400003-B5A3-F393-E0A9-E50E24DCCA9E - used to send data with  "NOTIFY"
+
+   The design of creating the BLE server is:
+   1. Create a BLE Server
+   2. Create a BLE Service
+   3. Create a BLE Characteristic on the Service
+   4. Create a BLE Descriptor on the characteristic
+   5. Start the service.
+   6. Start advertising.
+
+   In this example rxValue is the data received (only accessible inside that function).
+   And txValue is the data to be sent, in this example just a byte incremented every second. 
+*/
+#include <BLEDevice.h>
+#include <BLEServer.h>
+#include <BLEUtils.h>
+#include <BLE2902.h>
+String value;
+int i=0;
+String angles[6];
+BLECharacteristic *pCharacteristic;
+bool deviceConnected = false;
+int txValue = 0;
+int txValue1=0;
+const int readPin0 = 15; // Use GPIO number. See ESP32 board pinouts
+const int readPin1=4;
+
+const int LED = 2; // Could be different depending on the dev board. I used the DOIT ESP32 dev board.
+
+//std::string rxValue; // Could also make this a global var to access it in loop()
+
+// See the following for generating UUIDs:
+// https://www.uuidgenerator.net/
+
+#define SERVICE_UUID           "6E400001-B5A3-F393-E0A9-E50E24DCCA9E" // UART service UUID
+#define CHARACTERISTIC_UUID_RX "6E400002-B5A3-F393-E0A9-E50E24DCCA9E"
+#define CHARACTERISTIC_UUID_TX "6E400003-B5A3-F393-E0A9-E50E24DCCA9E"
+
+class MyServerCallbacks: public BLEServerCallbacks {
+    void onConnect(BLEServer* pServer) {
+      deviceConnected = true;
+    };
+
+    void onDisconnect(BLEServer* pServer) {
+      deviceConnected = false;
+    }
+};
+
+class MyCallbacks: public BLECharacteristicCallbacks {
+    void onWrite(BLECharacteristic *pCharacteristic) {
+      std::string rxValue = pCharacteristic->getValue();
+
+      if (rxValue.length() > 0) {
+        value="";
+//        Serial.println("*********");
+//        Serial.print("Received Value: ");
+
+        for (int i = 0; i < rxValue.length(); i++) {
+          //Serial.print(rxValue[i]);
+          value=value+rxValue[i];
+        }
+//        Serial.print("Recived Value=");
+//        Serial.print(value);
+//
+//        Serial.println();
+
+        // Do stuff based on the command received from the app
+        if (rxValue.find("A") != -1) { 
+          Serial.print("Turning ON!");
+          digitalWrite(LED, HIGH);
+        }
+        else if (rxValue.find("B") != -1) {
+          Serial.print("Turning OFF!");
+          digitalWrite(LED, LOW);
+        }
+
+        Serial.println();
+        Serial.println("*********");
+      }
+    }
+};
+
+void setup() {
+  Serial.begin(115200);
+
+  pinMode(LED, OUTPUT);
+
+  // Create the BLE Device
+  BLEDevice::init("ESP32 UART Test"); // Give it a name
+
+  // Create the BLE Server
+  BLEServer *pServer = BLEDevice::createServer();
+  pServer->setCallbacks(new MyServerCallbacks());
+
+  // Create the BLE Service
+  BLEService *pService = pServer->createService(SERVICE_UUID);
+
+  // Create a BLE Characteristic
+  pCharacteristic = pService->createCharacteristic(
+                      CHARACTERISTIC_UUID_TX,
+                      BLECharacteristic::PROPERTY_NOTIFY
+                    );
+                      
+  pCharacteristic->addDescriptor(new BLE2902());
+
+  BLECharacteristic *pCharacteristic = pService->createCharacteristic(
+                                         CHARACTERISTIC_UUID_RX,
+                                         BLECharacteristic::PROPERTY_WRITE
+                                       );
+
+  pCharacteristic->setCallbacks(new MyCallbacks());
+
+  // Start the service
+  pService->start();
+
+  // Start advertising
+  pServer->getAdvertising()->start();
+  Serial.println("Waiting a client connection to notify...");
+}
+
+void loop() {
+  if (deviceConnected) {
+    // Fabricate some arbitrary junk for now...
+    txValue = analogRead(readPin0);// This could be an actual sensor reading!
+    txValue1 = analogRead(readPin1);
+//    Serial.print(txValue);
+//    Serial.print(",");
+//    Serial.println(txValue1);
+//    char testString[2];
+//    testString[0]=txValue;
+//    testString[1]=0;
+//    testString[2]=txValue1;
+
+    // Let's convert the value to a char array:
+//   char txString[2]; // make sure this is big enuffz
+//  dtostrf(txValue, 1, 2, txString); // float_val, min_width, digits_after_decimal, char_buffer
+//    char txString1[2];
+// dtostrf(txValue1,1,2,txString1);
+    char dataString [9];
+   sprintf(dataString,"%d,%d",txValue,txValue1);
+  //  sprintf(dataString,"%d,%d",txValue,txValue1);
+//    pCharacteristic->setValue(&txValue, 1); // To send the integer value
+//    pCharacteristic->setValue("Hello!"); // Sending a test message
+    pCharacteristic->setValue(dataString);
+    
+    pCharacteristic->notify(); // Send the value to the app!
+//    Serial.print("*** Sent Value: ");
+//    Serial.print(dataString);
+//    Serial.println(" ***");
+      for (int i = 0; i < 2; i++)  //this for loop runs 10 times, bus just the number of parts which are possible will be saved in angles[i]
+  {
+    angles[i] = GetStringPartAtSpecificIndex(value, '/', i); //give this function your string to split, your char to split and the index of the StringPart you want to get
+    
+    Serial.print(angles[i]);
+    if(i==0)
+    {
+    Serial.print("/");  
+    }
+  }
+  Serial.println();
+
+  }
+  delay(1000);
+}
+String GetStringPartAtSpecificIndex(String StringToSplit, char SplitChar, int StringPartIndex)
+{
+  String originallyString = StringToSplit;
+  String outString = "";
+  for (int i1 = 0; i1 <= StringPartIndex; i1++)
+  {
+    outString = "";                   //if the for loop starts again reset the outString (in this case other part of the String is needed to take out)
+    int SplitIndex = StringToSplit.indexOf(SplitChar);  //set the SplitIndex with the position of the SplitChar in StringToSplit
+
+    if (SplitIndex == -1)               //is true, if no Char is found at the given Index
+    {
+      //outString += "Error in GetStringPartAtSpecificIndex: No SplitChar found at String '" + originallyString + "' since StringPart '" + (i1-1) + "'";    //just to find Errors
+      return outString;
+    }
+    for (int i2 = 0; i2 < SplitIndex; i2++)
+    {
+      outString += StringToSplit.charAt(i2);      //write the char at Position 0 of StringToSplit to outString
+    }
+    StringToSplit = StringToSplit.substring(StringToSplit.indexOf(SplitChar) + 1);  //change the String to the Substring starting at the position+1 where last SplitChar found
+  }
+  return outString;
+}
